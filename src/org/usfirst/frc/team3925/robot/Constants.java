835d16c1771ea8de67cc32ea1410560b6af8bda3
Avidh Bavkar <4bavkars@gmail.com>
2017-02-19 19:18:59 -0800

    Added Constants

---
+++ src/org/usfirst/frc/team3925/robot/Constants.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team3925.robot;
+
+public class Constants {
+	
+	public static final double DRIVETRAIN_WHEEL_DIAMETER = 4;
+	public static final int ENCODER_TICKS_PER_REV = 4096;
+
+}
