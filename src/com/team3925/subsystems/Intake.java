4d476a598e875e2eda7d0efb09780951ae6adfc7
Avidh Bavkar <4bavkars@gmail.com>
2017-03-23 20:01:25 -0700

    Populated intake subsystem

--- src/com/team3925/subsystems/Intake.java
+++ src/com/team3925/subsystems/Intake.java
@@ -1,8 +1,17 @@
 package com.team3925.subsystems;
 
+import com.ctre.CANTalon;
+import com.ctre.CanTalonJNI;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Intake extends Subsystem {
+	
+	DoubleSolenoid upDown;
+	
+	CANTalon wheels;
 	
 	private static Intake instance;
 	
@@ -13,7 +22,8 @@ public class Intake extends Subsystem {
 	}
 	
 	private Intake() {
-		
+		upDown = new DoubleSolenoid(0, 1);
+		wheels = new CANTalon(0);
 	}
 	
 	@Override
@@ -23,15 +33,15 @@ public class Intake extends Subsystem {
 	}
 	
 	public void setWheels(double percent) {
-		
+		wheels.set(percent);
 	}
 	
 	public void setDown() {
-		
+		upDown.set(Value.kForward);
 	}
 	
 	public void setUp() {
-		
+		upDown.set(Value.kReverse);
 	}
 	
 	public void setPiston(boolean isUp) {
