86e285997c0d730834dd8748984a14e77a64e874
Robotics <Robotics@172.31.145.179>
2017-03-24 18:11:59 -0700

    Populated OI, Robot Triggers Commands
    -OI stores Joysticks privately
    -Robot calls methods in OI to link Commands to JoystickButtons
    -Robot uses OI to schedule intake, shifting, climber commands

--- src/com/team3925/robot/OI.java
+++ src/com/team3925/robot/OI.java
@@ -1,12 +1,75 @@
 package com.team3925.robot;
 
+import java.util.HashMap;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	
-	private OI() {
+
+	private static OI instance;
+
+	private Joystick wheel, stick, xbox;
+
+	private HashMap<Integer, JoystickButton> wheelButtonTriggers;
+	private HashMap<Integer, JoystickButton> stickButtonTriggers;
+	private HashMap<Integer, JoystickButton> xboxButtonTriggers;
+
+	public static OI getInstance() {
+		if (instance == null)
+			instance = new OI();
+		return instance;
 	}
-	
+
+	private OI() {
+		wheel = new Joystick(0);
+		stick = new Joystick(1);
+		xbox = new Joystick(2);
+
+		wheelButtonTriggers = new HashMap<>();
+		stickButtonTriggers = new HashMap<>();
+		xboxButtonTriggers = new HashMap<>();
+	}
+
+	public void whenWheelButtonPressed(int button, Command command) {
+		if (!wheelButtonTriggers.containsKey(button))
+			wheelButtonTriggers.put(button, new JoystickButton(wheel, button));
+		wheelButtonTriggers.get(button).whenActive(command);
+	}
+
+	public void whenWheelButtonReleased(int button, Command command) {
+		if (!wheelButtonTriggers.containsKey(button))
+			wheelButtonTriggers.put(button, new JoystickButton(wheel, button));
+		wheelButtonTriggers.get(button).whenInactive(command);
+	}
+
+	public void whenStickButtonPressed(int button, Command command) {
+		if (!stickButtonTriggers.containsKey(button))
+			stickButtonTriggers.put(button, new JoystickButton(stick, button));
+		stickButtonTriggers.get(button).whenActive(command);
+	}
+
+	public void whenStickButtonReleased(int button, Command command) {
+		if (!stickButtonTriggers.containsKey(button))
+			stickButtonTriggers.put(button, new JoystickButton(stick, button));
+		stickButtonTriggers.get(button).whenInactive(command);
+	}
+
+	public void whenXboxButtonPressed(int button, Command command) {
+		if (!xboxButtonTriggers.containsKey(button))
+			xboxButtonTriggers.put(button, new JoystickButton(xbox, button));
+		xboxButtonTriggers.get(button).whenActive(command);
+	}
+
+	public void whenXboxButtonReleased(int button, Command command) {
+		if (!xboxButtonTriggers.containsKey(button))
+			xboxButtonTriggers.put(button, new JoystickButton(xbox, button));
+		xboxButtonTriggers.get(button).whenInactive(command);
+	}
+
 }
--- src/com/team3925/robot/Robot.java
+++ src/com/team3925/robot/Robot.java
@@ -1,6 +1,15 @@
 
 package com.team3925.robot;
 
+import com.team3925.commands.ClimberDisengage;
+import com.team3925.commands.ClimberEngage;
+import com.team3925.commands.DriveTrainShiftHigh;
+import com.team3925.commands.DriveTrainShiftLow;
+import com.team3925.commands.IntakeGoDown;
+import com.team3925.commands.IntakeGoUp;
+import com.team3925.commands.IntakeWheelsIn;
+import com.team3925.commands.IntakeWheelsOff;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
@@ -12,52 +21,64 @@ import edu.wpi.first.wpilibj.command.Scheduler;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+
+	private OI oi;
+
 	@Override
 	public void robotInit() {
-		
+		oi = OI.getInstance();
 	}
-	
+
 	@Override
 	public void disabledInit() {
 	}
-	
+
 	@Override
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
-	
+
 	@Override
 	public void autonomousInit() {
 	}
-	
+
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
 	}
-	
+
 	@Override
 	public void teleopInit() {
+		// intake controls
+		oi.whenXboxButtonPressed(0, IntakeGoDown.getInstance());
+		oi.whenXboxButtonPressed(0, IntakeWheelsIn.getInstance());
+		oi.whenXboxButtonReleased(0, IntakeGoUp.getInstance());
+		oi.whenXboxButtonReleased(0, IntakeWheelsOff.getInstance());
+
+		oi.whenXboxButtonPressed(0, IntakeGoDown.getInstance());
+		oi.whenXboxButtonReleased(0, IntakeGoUp.getInstance());
+
+		oi.whenXboxButtonPressed(0, IntakeWheelsIn.getInstance());
+		oi.whenXboxButtonReleased(0, IntakeWheelsOff.getInstance());
+
+		// shifting controls
+		oi.whenWheelButtonPressed(0, DriveTrainShiftHigh.getInstance());
+		oi.whenWheelButtonPressed(0, DriveTrainShiftLow.getInstance());
+
+		// climbing controls
+		oi.whenWheelButtonPressed(0, ClimberEngage.getInstance());
+		oi.whenWheelButtonPressed(0, ClimberDisengage.getInstance());
 	}
-	
-	/**
-	 * This function is called periodically during operator control
-	 */
+
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
 	}
-	
+
 	@Override
 	public void testInit() {
 	}
-	/**
-	 * This function is called periodically during test mode
-	 */
+
 	@Override
 	public void testPeriodic() {
 	}
